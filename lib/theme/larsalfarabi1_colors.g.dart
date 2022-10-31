48a7fd5d988e886cc26f16a11f617ff40d6f0f26
Parabeac-Nest <admin@parabeac.com>
2022-10-31 07:14:04 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/larsalfarabi1_colors.g.dart
@@ -0,0 +1,66 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class Larsalfarabi1Colors {
+
+static const Color putih = Color(0xffffffff);
+
+static const Color systemBgDarkBasePrimary = Color(0xff000000);
+
+static const Color labelLightPrimary = Color(0xff000000);
+
+static const Color labelDarkPrimary = Color(0xffffffff);
+
+static const Color labelDarkTertiary = Color(0xffebebf5);
+
+static const Color colorLightGreen = Color(0xff34c759);
+
+static const Color colorDarkYellow = Color(0xffffd60a);
+/// class var systemGreen: UIColor
+static const Color systemGreenLight = Color(0xff34c759);
+
+static const Color greyscale900 = Color(0xff212121);
+
+static const Color bgSeparator = Color(0xffdddddd);
+
+static const Color textDefault = Color(0xff3d6670);
+
+static const Color bgWhite = Color(0xffffffff);
+
+static const Color othersWhite = Color(0xffffffff);
+
+static const Color bgSecondary = Color(0xfff1f3f6);
+
+static const Color textDisable = Color(0xff94afb6);
+
+static const Color primaryBlue = Color(0xff404cb2);
+
+static const Color red = Color(0xffeb5757);
+
+static const Color moveIncome = Color(0xff41be06);
+
+static const Color moveSpending = Color(0xffeb1f39);
+
+static const Color primaryCloud = Color(0xffa8addd);
+
+static const Color primary500 = Color(0xff246bfd);
+
+static const Color greyscale200 = Color(0xffeeeeee);
+
+static const Color greyscale700 = Color(0xff616161);
+
+static const Color alerts&StatusError = Color(0xfff75555);
+
+static const Color secondary500 = Color(0xffffd300);
+
+static const Color alerts&StatusWarning = Color(0xfffacc15);
+
+static const Color othersBlack = Color(0xff000000);
+
+static const Color greyscale800 = Color(0xff424242);
+}
---
+++ lib/theme/larsalfarabi1_text_styles.g.dart
@@ -0,0 +1,125 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class Larsalfarabi1TextStyles {
+/// Previous style:
+body/large/bold
+static const TextStyle bodyLargeBold = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.20000000298023224,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle label13pt = TextStyle(
+  fontSize: 13,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Nunito',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle label11pt = TextStyle(
+  fontSize: 11,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0,
+  fontFamily: 'IBM Plex Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle label15pt = TextStyle(
+  fontSize: 15,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Nunito',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle volume16pt = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'IBM Plex Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle volume22pt = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.44,
+  fontFamily: 'IBM Plex Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyLargeMedium = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.20000000298023224,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Previous style:
+body/xlarge/semibold
+static const TextStyle bodyXlargeSemibold = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0.20000000298023224,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Previous style:
+body/xsmall/semibold
+static const TextStyle bodyXsmallSemibold = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0.20000000298023224,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Previous style:
+H4/bold
+static const TextStyle h4Bold = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Previous style:
+body/large/semibold
+static const TextStyle bodyLargeSemibold = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0.20000000298023224,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
